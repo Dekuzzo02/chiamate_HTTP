613138d5aa5db470d4efb221cc574180fd9e5124
Dekuzzo02 <dicuzzo8@gmail.com>
2020-02-05 13:55:27 +0000

    primo

---
+++ http/src/app/foo/foo.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-foo',
+  templateUrl: './foo.component.html',
+  styleUrls: ['./foo.component.css']
+})
+export class FooComponent implements OnInit {
+  data: Object;
+   loading: boolean;
+   o :Observable<Object>;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
